c47ce31609d087767c318eb9987364b60af208ca
Norman Alié <mail@normanalie.fr>
2022-01-22 11:58:21 +0100

    Add config.py file

---
+++ app/config.py
@@ -0,0 +1,14 @@
+"""Flask DEVELOPMENT configuration."""
+"""
+To start app: 
+    set FLASK_APP=app
+    set FLASK_DEBUG=True
+    set FLASK_ENV=development
+    flask run
+"""
+
+
+TESTING = True
+DEBUG = True
+DEVELOPMENT = True
+SECRET_KEY = '247MKgKxgc'
