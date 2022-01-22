7ed9be13a5c496da9dcbcfc6a3027cdc30b8f837
Norman Alié <mail@normanalie.fr>
2022-01-22 16:03:04 +0100

    Add user login to the login route

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from flask import redirect, render_template, url_for
+from flask_login import current_user, login_user
 
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/')
@@ -18,7 +20,18 @@ def mailing():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    errors = []
+    if current_user.is_authenticated:
+        return redirect(url_for(''))
+
     form = LoginForm()
-    if form.validate_on_submit():
-        return redirect(url_for('/'))
-    return render_template('login.html', form=form)
+    if form.validate_on_submit():  # POST processing
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None:
+            errors.append("Pas d'utilisateurs avec cet email")
+        elif not user.check_password(form.password.data):
+            errors.append("Mot de passe incorrect")
+        else:
+            login_user(user, remember=form.remember_me.data)
+            return redirect(url_for(''))
+    return render_template('login.html', form=form, errors=errors)
