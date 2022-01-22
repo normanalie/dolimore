61a2d502a73f1c9905cee7f0be5bcbfde550f386
Norman Alié <mail@normanalie.fr>
2022-01-22 15:46:49 +0100

    Update login form to use email instead of username

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Rester connecté", validators=[DataRequired()])
     submit = SubmitField("Se connecter")
