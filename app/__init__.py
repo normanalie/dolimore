26d193d551d8b7428073e584e4113fe5fe674e1b
Norman Alié <mail@normanalie.fr>
2022-02-06 09:11:14 +0100

    Add: flask-admin

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_admin import Admin
 
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
@@ -12,4 +13,16 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
+
 from app import routes, models
+
+
+# ADMIN PANEL
+from app.views import AdminIndexView
+from flask_admin.contrib.sqla import ModelView
+from app.models import User
+
+
+admin = Admin(app, index_view=AdminIndexView())
+
+admin.add_view(ModelView(User, db.session))
