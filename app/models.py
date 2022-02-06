1f82e3183fe1730c906af82e577f1735b4c6f5c4
Norman Alié <mail@normanalie.fr>
2022-02-06 09:11:31 +0100

    Add: is_admin in user model

--- app/models.py
+++ app/models.py
@@ -4,11 +4,13 @@ from flask_login import UserMixin
 from app import db, login
 
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(128), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    is_admin = db.Column(db.Boolean, default=False, nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
