92549651d7970825932c0c388ebb264d01261036
Norman Alié <mail@normanalie.fr>
2022-02-08 18:28:21 +0100

    Add: department form

--- app/mailing/forms.py
+++ app/mailing/forms.py
@@ -4,11 +4,15 @@ from wtforms import (
     BooleanField, 
     SubmitField, 
     )
+
+from .department import Department
+
 class MailingForm(FlaskForm):
     categories_customer = SelectMultipleField("Catégories Tiers")
     categories_contact = SelectMultipleField("Catégories Contact")
     operator_customer = RadioField("Filtre Tiers", choices=[("and", "ET"), ("or", "OU")], default='and')
     operator_contact = RadioField("Filtre Contact", choices=[("and", "ET"), ("or", "OU")], default='and')
+    departments_customer = SelectMultipleField("Departements Tiers", choices=Department.tuple("#n (#i)"))
     add_customer_contacts = BooleanField("Ajouter les contacts des tiers ?")
     submit = SubmitField("Ajouter")
     delete = SubmitField("Vider")
